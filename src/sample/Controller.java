ea7aa3ca39094c24d377d800b3c5056950cdb240
mkurmarc <51379562+mkurmarc@users.noreply.github.com>
2020-06-16 20:57:16 -0700

    completed main screen fxml. Added addPart.fxml file, AddPartController.java file

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,37 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import java.util.List;
+
 public class Controller {
+
+    private List<Part> partId;
+
+    private List<Product> productId;
+
+    private List<Part> partName;
+
+    private List<Product> productName;
+
+    private List<Part> partsInInventory;
+
+    private List<Product> productsInInventory;
+
+    private Label partIdLabel;
+
+    private Label partNameLabel;
+
+    private Label productNameLabel;
+
+//    @FXML
+//    public void handleAddPart() {
+//
+//    }
+
+//    public void initialize() {
+//    }
+
+
 }
